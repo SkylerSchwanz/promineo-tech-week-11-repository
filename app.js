12f702424a20a927cbe7c4187635bd9d0cbf5b9f
Skyler Schwanz <ghostsky10@gmail.com>
2023-08-15 21:10:05 -0700

    Completed Project

--- app.js
+++ app.js
@@ -1,50 +1,59 @@
 import { Player } from './classes/player.js';
 
-let Board = $('#board');
 let Boxes = $('#board .row').children().get();
 let WinnerBox = $('#winner');
-let WinnerScores = $('#scores').get();
-let Player1Score = WinnerScores[0];
-let Player2Score = WinnerScores[2];
 
-/*let Solutions = [6, 12, 15, 18, 24]*/;
+let Players = new Array(new Player($('#Player1').text(), 'O'), new Player($('#Player2').text(), 'X'));
+
 let Solutions = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9],
-  [1,4,7],
-  [2,5,8],
-  [3,6,9],
-  [1,5,9],
-  [3,5,7]
-]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
 
-let WinnerElement = function(player) {
+let WinnerElement = function (player) {
   return `
     <div class="alert alert-primary col-sm-8 col-lg-3 m-0" role="alert">
-      <h3 class="text-center">${ player.Name } Has Won The Game!</h3>
+      <h3 class="text-center">${player.Name} Has Won The Game!</h3>
     </div>
   `;
 };
 
-let DrawElement = function() {
+let PlayButton = function () {
+  return `
+    <div id="play" class="btn btn-dark rounded rounded-pill col-sm-6 col-lg-2">
+      <h2>Play Again</h2>
+    </div>
+  `;
+};
+
+let DrawElement = function () {
   return `
   <div class="alert alert-primary col-sm-8 col-lg-3 m-0" role="alert">
     <h3 class="text-center">Draw!</h3>
   </div>
   `;
-}
+};
 
 /*WinnerBox.empty();
 WinnerBox.append(WinnerElement[0]);*/
 
 class game {
   constructor() {
-    this.Players = new Array(new Player($('#Player1').text(), 'O'), new Player($('#Player2').text(), 'X'));
     this.Turn = undefined;
     this.Winner = undefined;
 
-    WinnerBox.empty();
+    WinnerBox.innerHTML = '';
+    $('#play').css('display', 'none');
+    this.#clearBoxes();
+    $('#winner').empty();
+    Players[0].ClickedBoxes = [];
+    Players[1].ClickedBoxes = [];
 
     $('#board .row')[0].addEventListener('click', (clicked) => {
       if (!Boxes.includes(clicked.target)) return;
@@ -62,19 +71,25 @@ class game {
 
   #switchTurns() {
     if (this.Turn === undefined) {
-      this.Turn = this.Players[0];
+      this.Turn = Players[0];
     } else {
-      if (this.Players.indexOf(this.Turn) == 0) {
-        this.Turn = this.Players[1];
+      if (Players.indexOf(this.Turn) == 0) {
+        this.Turn = Players[1];
       } else {
-        this.Turn = this.Players[0];
+        this.Turn = Players[0];
       }
     }
   }
 
+  #clearBoxes() {
+    Boxes.forEach((box) => {
+      box.innerHTML = '';
+    });
+  }
+
   #boxClicked(box) {
     let result = false;
-    this.Players.forEach((player) => {
+    Players.forEach((player) => {
       if (player.ClickedBoxes.includes(Boxes.indexOf(box) + 1)) {
         result = true;
       }
@@ -95,28 +110,35 @@ class game {
     player.Wins++;
 
     $('#winner').append(WinnerElement(player));
-  };
+    $('#play').css('display', 'inline');
+  }
 
   #findWinner() {
-    this.Players.forEach((player) => {
-      Solutions.forEach(solution => {
+    Players.forEach((player) => {
+      Solutions.forEach((solution) => {
         if (this.Winner !== undefined) return;
         let MappedBoxes = player.ClickedBoxes.sort().filter((i) => solution.includes(i));
         if (MappedBoxes.length === 3) {
           this.#Win(player);
         }
-      })
+      });
     });
 
-    if (this.Winner === undefined && this.Players[0].ClickedBoxes.length + this.Players[1].ClickedBoxes.length > 8) {
+    if (this.Winner === undefined && Players[0].ClickedBoxes.length + Players[1].ClickedBoxes.length > 8) {
       this.Winner = null;
+      $('#play').css('display', 'inline');
       $('#winner').append(DrawElement());
     }
-  };
-
-  #refreshWins() {
-
   }
 
+  #refreshWins() {
+    console.log($('#Player1Score').text());
+    $('#Player1Score').text(Players[0].Wins.toString());
+    $('#Player2Score').text(Players[1].Wins.toString());
+  }
 }
-new game();
+
+document.getElementById('play').addEventListener('click', (element) => {
+  new game();
+  element.target.innerHTML = '<h2>Play Again</h2>';
+});
